2e1d8ffd936481326ade17fb80c1f87e5db82615
Mykola <mykola.lisivka@gmail.com>
2018-11-13 23:00:54 +0200

    Added validations spec for Comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,4 +3,11 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to belong_to(:task) }
+
+  describe 'validations' do
+    it do
+      is_expected.to validate_presence_of(:body)
+        .with_message('The field is required.')
+    end
+  end
 end
